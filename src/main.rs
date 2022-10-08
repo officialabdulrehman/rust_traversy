4591a93ccacd59ce393728fc22576ae7ad4c5b40
NizTheDev <arehman08@hotmail.com>
2022-10-08 13:44:28 +0500

    tutorial complete

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,28 @@
 // mod datatypes;
 // mod print;
 // mod vars;
-mod strings;
+// mod strings;
+// mod vectors;
+// mod conditionals;
+// mod loops;
+// mod functions;
+// mod pointer_ref;
+// mod structs;
+// mod enums;
+// mod cli;
 
 fn main() {
     // print::run();
     // vars::run();
     // datatypes::run();
-    strings::run();
+    // strings::run();
+    // vectors::run();
+    // conditionals::run();
+    // loops::run();
+    // functions::run();
+    // pointer_ref::run();
+    // structs::run();
+    // enums::run();
+    // cli::run();
+    println!("Tutorial procrastinated successfully, hence failed. 🦀")
 }
--- src/print.rs
+++ src/print.rs
@@ -3,12 +3,12 @@ pub fn run() {
     println!("Hello from the print.rs file");
 
     // Basic Formatting
-    println!("{} is from {}", "Brad", "Mass");
+    println!("{} is from {}", "NizTheDev", "Mass");
 
     // Positional Arguments
     println!(
         "{0} is from {1} and {0} likes to {2}",
-        "Brad", "Mass", "code"
+        "NizTheDev", "Mass", "code"
     );
 
     // Named Arguments
